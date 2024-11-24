022d901b1b1bb54e62410f6cf9fbd28708d77203
Jean Paul <origemjhanpoll@gmail.com>
2024-11-24 15:02:34 -0300

    fist commit
    project init

---
+++ android/app/src/main/kotlin/com/example/yandeh_challenge/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.yandeh_challenge
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:yandeh_challenge/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
